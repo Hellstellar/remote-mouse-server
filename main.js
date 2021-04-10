34f207589327dff5e0ccd55b6e6d235ec3f7500e
sooraj <soorajdube.is@gmail.com>
2021-04-11 03:37:57 +0530

    Sooraj| Added the functionality to show app in tray

--- main.js
+++ main.js
@@ -1,28 +1,63 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, Tray } = require('electron')
 const createWebSocketServer = require("./src/services/web-socket");
 const path = require('path')
 require('dotenv').config();
 
+let tray, window
+
+app.dock.hide()
+
 function createWindow () {
-    const win = new BrowserWindow({
-        width: 800,
-        height: 600,
+    window = new BrowserWindow({
+        width: 420,
+        height: 560,
+        show: false,
+        frame: false,
+        fullscreenable: false,
         webPreferences: {
             preload: path.join(__dirname, 'preload.js'),
             nodeIntegration: true,
         }
     })
 
-    win.loadURL('http://localhost:3000')
+    window.on('closed', () => window = null)
+
+    window.loadURL('http://localhost:3000')
 
     //Create Websocket Server
     const port = process.env.WEBSOCKET_PORT;
-    createWebSocketServer(port, win.webContents)
+    createWebSocketServer(port,  window.webContents)
 }
+
+const createTray = () => {
+    tray = new Tray(path.join(__dirname, 'assets/mouse-outline.png'))
+    tray.on('click', () => toggleWindow())
+}
+
+const toggleWindow = () => {
+    window.isVisible() ? window.hide() : showWindow()
+}
+
+const showWindow = () => {
+    const position = windowPosition();
+    window.setPosition(position.x, position.y)
+    window.show()
+}
+
+const windowPosition = () => {
+    const windowBounds = window.getBounds();
+    const trayBounds = tray.getBounds();
+    const x = Math.round(trayBounds.x + (trayBounds.width / 2) - (windowBounds.width / 2))
+    const y = Math.round(trayBounds.y + trayBounds.height)
+    return { x, y }
+}
+
+
+
 
 app.whenReady().then(() => {
     createWindow()
-
+    createTray()
     app.on('activate', () => {
         if (BrowserWindow.getAllWindows().length === 0) {
             createWindow()
